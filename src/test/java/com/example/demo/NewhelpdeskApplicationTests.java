157b3b08d7e74b2349d4a5ed4663f68025676094
Mauro-vidal <mauroviidal@gmail.com>
2023-12-07 19:54:44 -0300

    Projeto iniciado.

---
+++ src/test/java/com/example/demo/NewhelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewhelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
